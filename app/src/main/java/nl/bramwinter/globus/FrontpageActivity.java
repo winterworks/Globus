1a2082a5e217d3b87eaae82c55625231bcd9df86
kristoffer_bak <team16203@gitswat.ase.au.dk>
2018-11-29 15:39:26 +0100

    more auth stuff

--- app/src/main/java/nl/bramwinter/globus/FrontpageActivity.java
+++ app/src/main/java/nl/bramwinter/globus/FrontpageActivity.java
@@ -1,16 +1,102 @@
 package nl.bramwinter.globus;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class FrontpageActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class FrontpageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth auth;
+    static final String TAG = "authentification";
+
+    private TextView statusTextView;
+    private EditText emailField;
+    private EditText passwordField;
+    private Button logIn;
+    private Button createUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
+
+        auth = FirebaseAuth.getInstance();
+
+        statusTextView = findViewById(R.id.textView3);
+        emailField = findViewById(R.id.editTextEmail);
+        passwordField = findViewById(R.id.editTextPassword);
+        logIn = findViewById(R.id.buttonLogin);
+        createUser = findViewById(R.id.buttonCreate);
+
+
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            ((TextView) findViewById(R.id.textView3)).setText(
+                    "User ID: " + user.getUid());
+        } else {
+            ((TextView) findViewById(R.id.textView3)).setText(
+                    "Error: sign in fucking failed.");
+        }
+    }
+
+    public void createAccount(String email, String password){
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(FrontpageActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.buttonCreate) {
+            createAccount(emailField.getText().toString(), passwordField.getText().toString());
+        } else if (i == R.id.buttonLogin) {
+            signIn(mEmailField.getText().toString(), passwordField.getText().toString());
+        } else if (i == R.id.signOutButton) {
+            signOut();
+        } else if (i == R.id.verifyEmailButton) {
+            sendEmailVerification();
+        }
+    }
 
 
 }
