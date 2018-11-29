808a73f2c820a4bd165bbf85312c02757d2f96d7
Z3W <emilcelik@gmail.com>
2018-11-29 16:06:44 +0100

    Add Firebasa Controller class

--- app/src/main/java/nl/bramwinter/globus/OverviewActivity.java
+++ app/src/main/java/nl/bramwinter/globus/OverviewActivity.java
@@ -18,6 +18,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
