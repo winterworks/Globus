cc7661b9b691d2ef41aa82bf94244b43d8da7662
kristoffer_bak <team16203@gitswat.ase.au.dk>
2018-12-06 14:42:45 +0100

    facebook auth now works

--- app/src/main/java/nl/bramwinter/globus/FrontpageActivity.java
+++ app/src/main/java/nl/bramwinter/globus/FrontpageActivity.java
@@ -13,6 +13,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -23,13 +30,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+
 /**
  *
- * Parts of this code is taken/inspired from Googles firebase guide
+ * Parts of this code is taken/inspired from Googles firebase guide and Facebooks authentication guide
  *
  */
 
@@ -47,13 +56,25 @@ public class FrontpageActivity extends AppCompatActivity implements View.OnClick
     private Button logIn;
     private Button createUser;
     private SignInButton googleLogin;
+    private LoginButton facebookLogin;
     GoogleSignInOptions gso;
     GoogleSignInClient googleSignInClient;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
+
+        //FAcebooks sdk's app activation helper:
+      /**
+       * FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+       **/
+
+      //facebook callback manager
+        callbackManager = CallbackManager.Factory.create();
 
         auth = FirebaseAuth.getInstance();
 
@@ -63,6 +84,28 @@ public class FrontpageActivity extends AppCompatActivity implements View.OnClick
         logIn = findViewById(R.id.buttonLogin);
         createUser = findViewById(R.id.buttonCreate);
         googleLogin = findViewById(R.id.googlelogin);
+        facebookLogin = findViewById(R.id.FB_login_button);
+
+        facebookLogin.setReadPermissions("email", "public_profile");
+        facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
 
         findViewById(R.id.googlelogin).setOnClickListener(this);
         findViewById(R.id.buttonCreate).setOnClickListener(this);
@@ -136,6 +179,32 @@ public class FrontpageActivity extends AppCompatActivity implements View.OnClick
         }
 
         return valid;
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(FrontpageActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     private void signInManually(String email, String password) {
@@ -212,6 +281,9 @@ public class FrontpageActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
